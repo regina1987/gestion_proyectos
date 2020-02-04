cd2807d48f6610729e7309c3c7a84ec6a7b6faf2
regiwa <mailofregina@gmail.com>
2020-02-04 12:41:11 -0300

    2 comit

--- app/models/proyecto.rb
+++ app/models/proyecto.rb
@@ -1,6 +1,6 @@
 class Proyecto < ApplicationRecord
   validates :name , presence: {message: 'empty'}
-  validates :descripcion , presence: {message: 'empty'}
+
   validates :begin_at , presence: {message: 'empty'}
   validates :end_at , presence: {message: 'empty'}
 
